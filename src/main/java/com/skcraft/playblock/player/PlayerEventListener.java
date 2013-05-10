82d7ed64fac341f37add703b2d3362834a84bf02
Albert Pham <the.sk89q@gmail.com>
2013-05-09 20:50:21 -0700

    Remove excessive number of console messages for the player.

--- src/main/java/com/skcraft/playblock/player/PlayerEventListener.java
+++ src/main/java/com/skcraft/playblock/player/PlayerEventListener.java
@@ -2,12 +2,11 @@ package com.skcraft.playblock.player;
 
 import java.util.logging.Level;
 
-import com.skcraft.playblock.PlayBlock;
-
 import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
 import uk.co.caprica.vlcj.player.MediaPlayer;
 import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter;
-import cpw.mods.fml.common.FMLLog;
+
+import com.skcraft.playblock.PlayBlock;
 
 /**
  * Logs semi-useful debugging messages to console.
@@ -28,31 +27,6 @@ public class PlayerEventListener extends MediaPlayerEventAdapter {
     @Override
     public void error(MediaPlayer mediaPlayer) {
         PlayBlock.log(Level.INFO, "Media has encountered an error!");
-    }
-
-    @Override
-    public void finished(MediaPlayer mediaPlayer) {
-        PlayBlock.log(Level.INFO, "Media has finished");
-    }
-
-    @Override
-    public void opening(MediaPlayer mediaPlayer) {
-        PlayBlock.log(Level.INFO, "Media is opening...");
-    }
-
-    @Override
-    public void paused(MediaPlayer mediaPlayer) {
-        PlayBlock.log(Level.INFO, "Media has paused");
-    }
-
-    @Override
-    public void playing(MediaPlayer mediaPlayer) {
-        PlayBlock.log(Level.INFO, "Media is playing");
-    }
-
-    @Override
-    public void stopped(MediaPlayer mediaPlayer) {
-        PlayBlock.log(Level.INFO, "Media stopped");
     }
 
     @Override
